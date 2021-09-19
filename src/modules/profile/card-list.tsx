68e7ace4e99dfa88bf40d23f16473ece87bde00c
xmaxinex4 <maxinegerhard@gmail.com>
2021-09-19 00:23:28 -0400

    adding profile works with pagination

--- src/modules/profile/card-list.tsx
+++ src/modules/profile/card-list.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { slice } from "lodash";
 
@@ -6,7 +6,6 @@ import { Button, Grid, makeStyles } from "@material-ui/core";
 import { Pagination } from "@material-ui/lab";
 
 import { ProfileModal } from "./form/profile-modal";
-
 import { ProfileCard } from "./card";
 import { selectProfiles } from "./slice";
 
@@ -30,16 +29,25 @@ export function ProfleCardList(props: ProfileCardListProps): React.ReactElement 
   const { profileCard, profileCardContainer } = useStyles();
 
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
   const [profilesToShow, setProfilesToShow] = useState(
-    slice(profiles, 0, profiles?.length < pageSize ? profiles.length : pageSize)
+    slice(profiles, 0, pageSize)
   );
-  
-  const onPageChange = useCallback((event: object, page: number) => {
+
+  // States to handle page navigation
+  const [page, setPage] = useState(1);
+
+  // Logic for handling which profiles to show given page and pageSize as profile list or page change
+  useEffect(() => {
     const lowRangeSlice = pageSize * (page - 1);
     const highRangeSlice = pageSize * page;
 
-    setProfilesToShow(slice(profiles, lowRangeSlice, highRangeSlice));
-  }, [pageSize, profiles]);
+    setProfilesToShow(slice(profiles, lowRangeSlice, highRangeSlice))    
+  }, [profiles, page, pageSize]);
+
+  const onPageChange = useCallback((event: object, page: number) => {
+    setPage(page);
+  }, [setPage]);
 
   const pageCount = useMemo(() => {
     let count = Math.floor(profiles.length / pageSize);
@@ -66,7 +74,7 @@ export function ProfleCardList(props: ProfileCardListProps): React.ReactElement 
       </Grid>
       <Grid item container className={profileCardContainer} spacing={2} alignItems="center" justifyContent="center">
         {profilesToShow.map(profile => (
-          <Grid item className={profileCard} xs={12} md={6}>
+          <Grid item className={profileCard} xs={12} md={6} key={profile.id}>
             <ProfileCard profile={profile} />
           </Grid>
         ))}
